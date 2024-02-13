0c44f28c37d4b1ce39c77fdf2ed57b1e11024604
charlesoller <charlesrello@gmail.com>
2024-02-12 19:01:31 -0500

    Add User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,7 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Sign up
 router.post(
     '/',
     async (req, res) => {
@@ -26,5 +27,5 @@ router.post(
       });
     }
   );
-  
+
 module.exports = router;
