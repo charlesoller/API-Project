d6ca70b19d2ea3cbf0a10a657b22fd626da50619
charlesoller <charlesrello@gmail.com>
2024-02-20 20:50:31 -0500

    Added one to many associations for Spots to SpotImages, and for Reviews to ReviewImages. Also Altered the migration files for SpotImage and ReviewImage to reflect this change.

--- backend/db/migrations/20240221004010-create-spot-image.js
+++ backend/db/migrations/20240221004010-create-spot-image.js
@@ -10,7 +10,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       spotId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Spot',
+          key: 'id'
+        }
       },
       url: {
         type: Sequelize.STRING
