783ee6c40cc317611258599940f7cd0756434069
charlesoller <charlesrello@gmail.com>
2024-02-20 20:30:34 -0500

    added seeder for SpotImages

---
+++ backend/db/seeders/20240221012907-seed-spot-images.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { SpotImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await SpotImage.bulkCreate([
+      {
+        spotId: 1,
+        url: "Spot Image 1"
+      },
+      {
+        spotId: 2,
+        url: "Spot Image 2"
+      },
+      {
+        spotId: 3,
+        url: "Spot Image 3"
+      },
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
