23acc578eb2d9147d80e3a86b4917d8f1e42ddc9
charlesoller <charlesrello@gmail.com>
2024-02-21 17:46:26 -0500

    Finished Spot Routes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,12 +2,18 @@ const express = require('express')
 const { Op } = require('sequelize');
 const { Spot, SpotImage, User } = require('../../db/models');
 const router = express.Router();
-const sequelize = require('sequelize')
+const sequelize = require('sequelize');
+const spot = require('../../db/models/spot');
+
+/* ==============================================================================================================
+                                                GET ROUTES
+============================================================================================================== */
 
 // Get all Spots
 router.get("/", async(req, res) => {
     const spots = await Spot.findAll({
         raw: true,
+        //The inclusion of all attributes as seen below is necessary for getting rid of the model name of SpotImages in the response
         attributes: [
             'id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'description', 'price', 'createdAt', 'updatedAt', 'avgRating',
             [sequelize.col('SpotImages.url'), 'previewImage']
@@ -95,6 +101,10 @@ router.get("/:id", async(req, res) => {
     return res.json(spot)
 })
 
+/* ==============================================================================================================
+                                                POST ROUTES
+============================================================================================================== */
+
 // Create a Spot
 router.post("/", async(req, res) => {
     const { address, city, state, country, lat, lng, name, description, price } = req.body
@@ -128,6 +138,113 @@ router.post("/", async(req, res) => {
         err.errors = errors
         return res.status(400).json(err)
     }
+})
+
+router.post("/:id/images", async(req, res) => {
+    const { url, preview } = req.body
+    const { id } = req.params
+    const userId = req.user?.id
+    const spot = await Spot.findByPk(id, { raw: true })
+
+    if(!spot){
+        return res.status(404).json({ message: "Spot couldn't be found" })
+    }
+
+    if(!userId || spot.ownerId !== userId){
+        return res.status(404).json({ message: "You are not authorized to add an image to this spot." })
+    }
+
+    const spotImage = await SpotImage.create({ spotId: id, url, preview })
+    return res.json({
+        id: spotImage.id,
+        url: spotImage.url,
+        preview: spotImage.preview
+    })
+})
+
+/* ==============================================================================================================
+                                                PUT ROUTES
+============================================================================================================== */
+
+router.put("/:id", async(req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+    const { id } = req.params
+    const userId = req.user?.id
+    const spot = await Spot.findByPk(id)
+
+    if(!spot){
+        return res.status(404).json({ message: "Spot couldn't be found" })
+    }
+
+    if(!userId || spot.ownerId !== userId){
+        return res.status(404).json({ message: "You are not authorized to update this spot." })
+    }
+
+    try {
+        spot.set({
+            address, city, state, country, lat, lng, name, description, price
+        })
+        await spot.save()
+        return res.json({
+            // This is to exclude numReviews and avgRating
+            id: spot.id,
+            ownerId: spot.ownerId,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt
+        })
+
+    } catch(e) {
+        const err = { message: "Bad Request" }
+        const errors = {}
+        e.errors.forEach(error => {
+            const errItem = error.path
+            switch (errItem) {
+                case 'address': errors.address = "Street address is required"; break;
+                case 'city': errors.city = "City is required"; break;
+                case 'state': errors.state = "State is required"; break;
+                case 'country': errors.country = "Country is required"; break;
+                case 'lat': errors.lat = "Latitude must be within -180 and 180"; break;
+                case 'lng': errors.lng = "Longitude must be within -180 and 180"; break;
+                case 'name': errors.name = "Name must be less than 50 characters"; break;
+                case 'description': errors.description = "Description is required"; break;
+                case 'price': errors.price = "Price per day must be a positive number"; break;
+                default: errors.default = "No error found. Please try again."; break;
+            }
+        })
+
+        err.errors = errors
+        return res.status(400).json(err)
+    }
+})
+
+/* ==============================================================================================================
+                                               DELETE ROUTES
+============================================================================================================== */
+
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params
+    const userId = req.user?.id
+    const spot = await Spot.findByPk(id)
+
+    if(!spot){
+        return res.status(404).json({ message: "Spot couldn't be found" })
+    }
+
+    if(!userId || spot.ownerId !== userId){
+        return res.status(404).json({ message: "You are not authorized to delete this spot." })
+    }
+
+    await spot.destroy()
+    res.json({ message: "Successfully deleted"})
 })
 
 module.exports = router;
