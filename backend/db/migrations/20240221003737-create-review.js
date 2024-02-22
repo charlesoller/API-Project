9292c87acaf888e4d81785c23b3e42a8d189b449
charlesoller <charlesrello@gmail.com>
2024-02-21 20:47:26 -0500

    attempting to fix

--- backend/db/migrations/20240221003737-create-review.js
+++ backend/db/migrations/20240221003737-create-review.js
@@ -41,7 +41,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }).t;
     await queryInterface.addIndex('Reviews', ['userId', 'spotId'])
   },
   async down(queryInterface, Sequelize) {
