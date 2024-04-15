0d096424640b1d3334a08b38c9e399eb3f34ccea
charlesoller <charlesrello@gmail.com>
2024-04-15 10:25:11 -0400

    Finished sign up form

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
 import * as sessionActions from '../../store/session';
-import './SignupForm.css';
+import styles from './SignupForm.module.css';
 
 function SignupFormModal() {
   const dispatch = useDispatch();
@@ -14,6 +14,17 @@ function SignupFormModal() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
+
+  // This function is only responsible for handling whether or not the button is disabled.
+  const validateFields = () => {
+    if(!email.length || !username.length || !firstName.length
+      || !lastName.length || !password.length || !confirmPassword.length
+      || username.length < 4 || password.length < 6) {
+      return true
+    } else {
+      return false
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -42,74 +53,80 @@ function SignupFormModal() {
   };
 
   return (
-    <>
+    <div className={styles.main}>
       <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label className={styles.input_group}>
           Email
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
+            className={styles.input}
           />
         </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
+        {errors.email && <p className={styles.error}>{errors.email}</p>}
+        <label className={styles.input_group}>
           Username
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
+            className={styles.input}
           />
         </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
+        {errors.username && <p className={styles.error}>{errors.username}</p>}
+        <label className={styles.input_group}>
           First Name
           <input
             type="text"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             required
+            className={styles.input}
           />
         </label>
-        {errors.firstName && <p>{errors.firstName}</p>}
-        <label>
+        {errors.firstName && <p className={styles.error}>{errors.firstName}</p>}
+        <label className={styles.input_group}>
           Last Name
           <input
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
             required
+            className={styles.input}
           />
         </label>
-        {errors.lastName && <p>{errors.lastName}</p>}
-        <label>
+        {errors.lastName && <p className={styles.error}>{errors.lastName}</p>}
+        <label className={styles.input_group}>
           Password
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            className={styles.input}
           />
         </label>
-        {errors.password && <p>{errors.password}</p>}
-        <label>
+        {errors.password && <p className={styles.error}>{errors.password}</p>}
+        <label className={styles.input_group}>
           Confirm Password
           <input
             type="password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
+            className={styles.input}
           />
         </label>
         {errors.confirmPassword && (
-          <p>{errors.confirmPassword}</p>
+          <p className={styles.error}>{errors.confirmPassword}</p>
         )}
-        <button type="submit">Sign Up</button>
+        <button type="submit" className={styles.button} disabled={validateFields()}>Sign Up</button>
       </form>
-    </>
+    </div>
   );
 }
 
